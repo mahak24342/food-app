f7cc39822e61d9ac20ce40ebcdf16886d2012e99
LENOVO <porwalmahak784@gmail.com>
2023-07-22 16:13:11 +0530

    first

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -18,12 +18,12 @@ const Menu = () => {
     return (
     <div>
         { !open ?(
-        <Image src="/open1.png" alt="" width={20} height={20}  onClick={()=>setOpen(true)}/>
+        <Image src="/open1.png" alt="" width={30} height={25}  onClick={()=>setOpen(true)}/>
    ):(
    <Image src="/close.png" alt="" width={20} height={20}   onClick={()=>setOpen(false)}/>
 )   }
    {open && (
-   <div className='bg-red-500 text-white absolute left-0 top-24 h-[calc(100vh-6rem)] flex flex-col pr-20 items-center justify-center text-3xl gap-8 '>
+   <div className='bg-red-500 text-white absolute right-0 top-24 h-[calc(100vh-6rem)] flex flex-col pr-20 items-center justify-center text-3xl gap-8 '>
     {links.map(item=>(
         <Link href={item.url} key={item.id} onClick={()=>setOpen(false)}>
             {item.title}
